5b48570d6a08ef27f928e8a1d6da9035ebc72b82
Ruben Lopez Ruiz <rlopezru@cern.ch>
2023-02-14 19:50:16 +0100

    Local reco modified

--- RecoLocalFastTime/FTLCommonAlgos/plugins/ETLUncalibRecHitAlgo.cc
+++ RecoLocalFastTime/FTLCommonAlgos/plugins/ETLUncalibRecHitAlgo.cc
@@ -12,8 +12,12 @@ public:
         adcSaturation_(conf.getParameter<double>("adcSaturation")),
         adcLSB_(adcSaturation_ / (1 << adcNBits_)),
         toaLSBToNS_(conf.getParameter<double>("toaLSB_ns")),
+        tocLSBToNS_(conf.getParameter<double>("tocLSB_ns")),
         tofDelay_(conf.getParameter<double>("tofDelay")),
-        timeError_(conf.getParameter<std::string>("timeResolutionInNs")) {}
+        timeError_(conf.getParameter<std::string>("timeResolutionInNs")),
+        timeCorr_p0_(conf.getParameter<double>("timeCorr_p0")),
+        timeCorr_p1_(conf.getParameter<double>("timeCorr_p1")),
+        timeCorr_p2_(conf.getParameter<double>("timeCorr_p2")) {}
 
   /// Destructor
   ~ETLUncalibRecHitAlgo() override {}
@@ -30,34 +34,48 @@ private:
   const double adcSaturation_;
   const double adcLSB_;
   const double toaLSBToNS_;
+  const double tocLSBToNS_;
   const double tofDelay_;
   const reco::FormulaEvaluator timeError_;
+  const double timeCorr_p0_;
+  const double timeCorr_p1_;
+  const double timeCorr_p2_;
 };
 
 FTLUncalibratedRecHit ETLUncalibRecHitAlgo::makeRecHit(const ETLDataFrame& dataFrame) const {
   constexpr int iSample = 2;  //only in-time sample
   const auto& sample = dataFrame.sample(iSample);
 
-  const std::array<double, 1> amplitudeV = {{double(sample.data()) * adcLSB_}};
-  // NB: Here amplitudeV is defined as an array in order to be used
-  //     below as an input to FormulaEvaluator::evaluate.
-  double time = double(sample.toa()) * toaLSBToNS_ - tofDelay_;
+  std::pair<float, float> amplitude(0., 0.);
+  std::pair<float, float> time(0., 0.);
+
+  amplitude.first = double(sample.data()) * adcLSB_;
+  double time_of_arrival = double(sample.toa()) * toaLSBToNS_ - tofDelay_;
+  double time_of_crossing = double(sample.toc()) * tocLSBToNS_ - tofDelay_;
+  double time_over_threshold = time_of_crossing - time_of_arrival;
   unsigned char flag = 0;
 
-  LogDebug("ETLUncalibRecHit") << "ADC+: set the charge to: " << amplitudeV[0] << ' ' << sample.data() << ' ' << adcLSB_
-                               << ' ' << std::endl;
-  LogDebug("ETLUncalibRecHit") << "ADC+: set the time to: " << time << ' ' << sample.toa() << ' ' << toaLSBToNS_ << ' '
-                               << std::endl;
-  LogDebug("ETLUncalibRecHit") << "Final uncalibrated amplitude : " << amplitudeV[0] << std::endl;
+  // Time-walk correction for toa
+  time.first -= timeCorr_p0_ * pow(time_over_threshold, timeCorr_p1_) + timeCorr_p2_;
+  flag |= 0x1;
 
+  LogDebug("ETLUncalibRecHit") << "ADC+: set the charge to: " << amplitude.first << ' ' << sample.data() << ' ' << adcLSB_
+                               << ' ' << std::endl;
+  LogDebug("ETLUncalibRecHit") << "ADC+: set the time to: " << time.first << ' ' << sample.toa() << ' ' << toaLSBToNS_ << ' '
+                               << std::endl;
+  LogDebug("ETLUncalibRecHit") << "Final uncalibrated amplitude : " << amplitude.first << std::endl;
+
+  // NB: Here amplitudeV is defined as an array in order to be used
+  //     below as an input to FormulaEvaluator::evaluate.
+  const std::array<double, 1> amplitudeV = {{amplitude.first}};
   const std::array<double, 1> emptyV = {{0.}};
   double timeError = timeError_.evaluate(amplitudeV, emptyV);
 
   return FTLUncalibratedRecHit(dataFrame.id(),
                                dataFrame.row(),
                                dataFrame.column(),
-                               {amplitudeV[0], 0.f},
-                               {time, 0.f},
+                               amplitude,
+                               time,
                                timeError,
                                -1.f,
                                -1.f,
