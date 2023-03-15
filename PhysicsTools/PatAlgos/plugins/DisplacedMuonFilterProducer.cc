16b6525c75188dd9b46aecbf73909acf640e6bf3
Ruben Lopez Ruiz <24.lopezr@gmail.com>
2023-03-15 12:25:09 +0100

    Preliminar fix to DisplacedMuonFilterProducer

--- PhysicsTools/PatAlgos/plugins/DisplacedMuonFilterProducer.cc
+++ PhysicsTools/PatAlgos/plugins/DisplacedMuonFilterProducer.cc
@@ -158,6 +158,8 @@ void pat::DisplacedMuonFilterProducer::produce(edm::Event& iEvent, const edm::Ev
   std::vector<bool> filteredmuons(nMuons, true);
   int oMuons = nMuons;
 
+  std::cout << "begin loop. minMatches = " << minMatches_ << std::endl;
+  unsigned int nsegments = 0;
   for (unsigned int i = 0; i < srcMuons->size(); i++) {
     const reco::Muon& muon(srcMuons->at(i));
 
@@ -170,8 +172,19 @@ void pat::DisplacedMuonFilterProducer::produce(edm::Event& iEvent, const edm::Ev
           continue;
         }
       } else {
+        // Compute number of DT+CSC segments
+        nsegments = 0;
+        for (trackingRecHit_iterator hit = muon.standAloneMuon()->recHitsBegin(); hit != muon.standAloneMuon()->recHitsEnd(); ++hit) {
+          if (!(*hit)->isValid()) continue;
+          DetId id = (*hit)->geographicalId();
+          if (id.det() != DetId::Muon) continue;
+          if (id.subdetId() == MuonSubdetId::DT || id.subdetId() == MuonSubdetId::CSC) {
+            nsegments++;
+          }
+        }
+        std::cout << "nsegments: " << nsegments << std::endl;
         // Discard STA-only muons with less than minMatches_ segments and below pt threshold
-        if (!muon.isMatchesValid() || muon.numberOfMatches() < minMatches_ || muon.standAloneMuon()->pt() < minPtSTA_) {
+        if (nsegments < minMatches_ || muon.standAloneMuon()->pt() < minPtSTA_) {
           filteredmuons[i] = false;
           oMuons = oMuons - 1;
           continue;
